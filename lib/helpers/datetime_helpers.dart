08a50428f3b4cf1c886e90496e91bf00e8050d8c
kda100 <65980399+kda100@users.noreply.github.com>
2022-03-09 13:06:09 +0000

    Update datetime_helpers.dart

--- lib/helpers/datetime_helpers.dart
+++ lib/helpers/datetime_helpers.dart
@@ -8,21 +8,23 @@ class DateTimeHelpers {
   static String getTimeAfterPost({required DateTime createdAt}) {
     final now = DateTime.now();
     final diff = now.millisecondsSinceEpoch - createdAt.millisecondsSinceEpoch;
-    if (diff < 60000) {
-      final int secs = (diff / 1000).ceil();
-      return "$secs seconds ago";
-    } else if (diff < 3600000) {
-      final int mins = (diff / 60000).ceil();
-      return "$mins minutes ago";
-    } else if (diff < 86400000) {
+    if(diff < 1000) { //less than 1 sec
+      return "now";
+    } else if (diff < 60000) { //less than 1 minute
+      final int secs = (diff / 1000).floor();
+      return secs > 1 ? "$secs seconds ago" : "$secs second ago";
+    } else if (diff < 3600000) { //less than 1 hour
+      final int mins = (diff / 60000).floor();
+      return mins > 1 ? "$mins minutes ago" : "$mins minute ago";
+    } else if (diff < 86400000) { //less than 1 day
       final int hours = (diff / 3600000).floor();
-      return "$hours hours ago";
-    } else if (diff < 604800000) {
+      return hours > 1 ? "$hours hours ago" : "$hours hour ago";
+    } else if (diff < 604800000) { // less than 1 week
       final int days = (diff / 86400000).floor();
-      return "$days days ago";
-    } else {
+      return days > 1 ? "$days days ago" : "$days day ago";
+    } else { //longer than a week.
       final int weeks = (diff / 604800000).floor();
-      return "$weeks weeks ago";
+      return weeks > 1 ? "$weeks weeks ago" : "$weeks week ago";
     }
   }
 }
