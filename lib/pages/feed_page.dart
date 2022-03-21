01c6726acc73285b489521ebaa2236a84c313c82
kda100 <65980399+kda100@users.noreply.github.com>
2022-03-21 08:13:46 +0000

    Update feed_page.dart

--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -2,8 +2,10 @@ import 'package:feed_tab_spotlas/models/feed_response.dart';
 import 'package:feed_tab_spotlas/providers/feed_provider.dart';
 import 'package:feed_tab_spotlas/providers/post_layout_provider.dart';
 import 'package:feed_tab_spotlas/widgets/post_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 ///page where feed containing posts are displayed. The feed is displayed as a list.
 ///Containing each post widget.
@@ -39,65 +41,76 @@ class _FeedPageState extends State<FeedPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Feed",
-          style: TextStyle(color: Colors.black),
-        ),
-      ),
-      body: Provider<PostLayoutProvider>(
-        create: (ctx) =>
-            PostLayoutProvider(deviceSize: MediaQuery.of(context).size),
-        child: Selector<FeedProvider, FeedResponse>(
-          //rebuilds on Feed response
-          selector: (context, feedProvider) => feedProvider.feedResponse,
-          builder: (context, feedResponse, _) {
-            final FeedResponseType feedResponseType =
-                feedResponse.feedResponseType;
-            if (feedResponseType == FeedResponseType.loading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              if (feedResponseType == FeedResponseType.error) {
-                Center(
-                  child: Text(feedResponse.message),
-                );
-              }
-              return RefreshIndicator(
-                onRefresh: () async {
-                  feedProvider.refreshFeed();
-                },
-                child: ListView.builder(
-                  //posts.
-                  cacheExtent: 100,
-                  controller: _controller,
-                  physics: const BouncingScrollPhysics(),
-                  itemCount: feedProvider.numberOfPosts + 1,
-                  itemBuilder: (context, index) {
-                    if (index != feedProvider.numberOfPosts) {
-                      return PostWidget(
-                        //widget is built when user scrolls to it.
-                        postViewModel: feedProvider.getPostViewModel(
-                          index:
-                              index, //uses index of List View to retrieve the required PostViewModel.
-                        ),
-                      );
-                    } else {
-                      return const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                      );
-                    }
-                  },
-                ),
+    final Widget scaffoldBody = Provider<PostLayoutProvider>(
+      create: (ctx) =>
+          PostLayoutProvider(deviceSize: MediaQuery.of(context).size),
+      child: Selector<FeedProvider, FeedResponse>(
+        //rebuilds on Feed response
+        selector: (context, feedProvider) => feedProvider.feedResponse,
+        builder: (context, feedResponse, _) {
+          final FeedResponseType feedResponseType =
+              feedResponse.feedResponseType;
+          if (feedResponseType == FeedResponseType.loading) {
+            return const Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          } else {
+            if (feedResponseType == FeedResponseType.error) {
+              Center(
+                child: Text(feedResponse.message),
               );
             }
-          },
+            return RefreshIndicator(
+              onRefresh: () async {
+                feedProvider.refreshFeed();
+              },
+              child: ListView.builder(
+                //posts.
+                cacheExtent: 100,
+                controller: _controller,
+                physics: const BouncingScrollPhysics(),
+                itemCount: feedProvider.numberOfPosts + 1,
+                itemBuilder: (context, index) {
+                  if (index != feedProvider.numberOfPosts) {
+                    return PostWidget(
+                      //widget is built when user scrolls to it.
+                      postViewModel: feedProvider.getPostViewModel(
+                        index:
+                            index, //uses index of List View to retrieve the required PostViewModel.
+                      ),
+                    );
+                  } else {
+                    return const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                        child: CircularProgressIndicator.adaptive(),
+                      ),
+                    );
+                  }
+                },
+              ),
+            );
+          }
+        },
+      ),
+    );
+
+    return PlatformScaffold(
+      cupertino: (_, __) => CupertinoPageScaffoldData(
+          navigationBar: const CupertinoNavigationBar(
+            middle: Text(
+              "Feed",
+            ),
+          ),
+          body: scaffoldBody),
+      material: (_, __) => MaterialScaffoldData(
+        appBar: AppBar(
+          title: const Text(
+            "Feed",
+            style: TextStyle(color: Colors.black),
+          ),
         ),
+        body: scaffoldBody,
       ),
     );
   }
