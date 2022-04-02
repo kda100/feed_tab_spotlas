fecce8976d50e30db497abceba34f92a2bc41a99
kda100 <65980399+kda100@users.noreply.github.com>
2022-04-02 11:30:04 +0100

    Delete lib directory

--- lib/services/status_codes.dart
+++
@@ -1,6 +0,0 @@
-///class contains status codes for Http requests.
-///More can be added, if more status code cases want to be controlled.
-
-class StatusCodes {
-  static const int success = 200;
-}
